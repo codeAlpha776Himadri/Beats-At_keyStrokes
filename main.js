2c8d14088e75bf8c5929d89a6f1ab1c24289f4bb
Himadri <hck364864@gmail.com>
2021-07-14 18:55:13 +0530

    Added screen page

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
-window.addEventListener('keydown' , (e) => {
-    let enteredKey = `code-${e.keyCode}`
-    // console.log(enteredKey) 
+const btn = document.querySelector('.screen>button');
+const screen = document.querySelector('.screen');
 
-    const audio = document.querySelector(`audio[data-key="${enteredKey}"]`)
-    const key = document.querySelector(`.${enteredKey}`)
+btn.style.cursor='pointer';
 
-    // console.log(audio)
-    // console.log(key)
+btn.addEventListener('click',() => {
+    screen.classList.add('screen-animation');
 
-    if(!audio){ return };
-    audio.currentTime = 0 ;
-    audio.play();
 
-    key.classList.add('key-box-animationOnClick')
+    window.addEventListener('keydown' , (e) => {
+        let enteredKey = `code-${e.keyCode}`
+    
+        const audio = document.querySelector(`audio[data-key="${enteredKey}"]`) ;
+        const key = document.querySelector(`.${enteredKey}`) ;
+    
+        if(!audio){ return };
+        audio.currentTime = 0 ; // to rewind with each click
+        audio.play();
+    
+        key.classList.add('key-box-animationOnClick') ;
+    
+        window.addEventListener('transitionend' , () => {
+            key.classList.remove('key-box-animationOnClick');
+        });
+    });
 
-    window.addEventListener('transitionend' , (e) => {
-        key.classList.remove('key-box-animationOnClick');
-    })
-})
+
+});
+
+// window.addEventListener('keydown' , (e) => {
+//     let enteredKey = `code-${e.keyCode}`
+
+//     const audio = document.querySelector(`audio[data-key="${enteredKey}"]`) ;
+//     const key = document.querySelector(`.${enteredKey}`) ;
+
+//     if(!audio){ return };
+//     audio.currentTime = 0 ; // to rewind with each click
+//     audio.play();
+
+//     key.classList.add('key-box-animationOnClick') ;
+
+//     window.addEventListener('transitionend' , () => {
+//         key.classList.remove('key-box-animationOnClick');
+//     });
+// });
+
