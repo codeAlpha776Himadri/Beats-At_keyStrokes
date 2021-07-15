917aeb7ee6b523534c1e2a002418c5f007f4cddf
Himadri <hck364864@gmail.com>
2021-07-15 20:31:18 +0530

    files formatted

--- main.js
+++ main.js
@@ -1,46 +1,27 @@
 const btn = document.querySelector('.screen>button');
 const screen = document.querySelector('.screen');
 
-btn.style.cursor='pointer';
+btn.style.cursor = 'pointer';
 
-btn.addEventListener('click',() => {
+btn.addEventListener('click', () => {
     screen.classList.add('screen-animation');
 
 
-    window.addEventListener('keydown' , (e) => {
+    window.addEventListener('keydown', (e) => {
         let enteredKey = `code-${e.keyCode}`
-    
-        const audio = document.querySelector(`audio[data-key="${enteredKey}"]`) ;
-        const key = document.querySelector(`.${enteredKey}`) ;
-    
-        if(!audio){ return };
-        audio.currentTime = 0 ; // to rewind with each click
+
+        const audio = document.querySelector(`audio[data-key="${enteredKey}"]`);
+        const key = document.querySelector(`.${enteredKey}`);
+
+        if (!audio) { return };
+        audio.currentTime = 0; // to rewind with each click
         audio.play();
-    
-        key.classList.add('key-box-animationOnClick') ;
-    
-        window.addEventListener('transitionend' , () => {
+
+        key.classList.add('key-box-animationOnClick');
+
+        window.addEventListener('transitionend', () => {
             key.classList.remove('key-box-animationOnClick');
         });
     });
 
-
 });
-
-// window.addEventListener('keydown' , (e) => {
-//     let enteredKey = `code-${e.keyCode}`
-
-//     const audio = document.querySelector(`audio[data-key="${enteredKey}"]`) ;
-//     const key = document.querySelector(`.${enteredKey}`) ;
-
-//     if(!audio){ return };
-//     audio.currentTime = 0 ; // to rewind with each click
-//     audio.play();
-
-//     key.classList.add('key-box-animationOnClick') ;
-
-//     window.addEventListener('transitionend' , () => {
-//         key.classList.remove('key-box-animationOnClick');
-//     });
-// });
-
